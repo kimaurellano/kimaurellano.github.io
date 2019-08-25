9e5d6242e23547556f5985977ebdfe01077fb4ba
kimaurellano <kim.aurellano@yahoo.com>
2019-08-25 12:38:36 +0800

    Updated Projects button behaviour

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,4 @@
 // Start js
 $(document).ready(() => {
-    let platform;
-    $('.c1').click(() => {
-        // Get id of the element clicked
-        platform = event.target.id.split('-')[0];
-        $('.platform-content').fadeIn(200);
-        $(`.${platform}-projects`).show();
-    });
-
-    $('.exit-button').click(() => { 
-        $('.platform-content').fadeOut(200);
-        console.log(platform);
-        $(`.platform-content > .${platform}-projects`).hide();
-    });
+    // TODO
 });
